43214086b81f0880686922b6e05f7eadfa7eb973
kinseyda <daniel.kinsey@mail.utoronto.ca>
2019-10-22 13:19:05 -0400

    Added setters and switchers to board and tile

--- board.py
+++ board.py
@@ -2,12 +2,18 @@ from tile import Tile
 class Board:
     
     def __init__(self):
-        self.board = [[tile.Tile()]*9]*9
+        self.board = [[Tile()]*9]*9
         # 9x9 board, list of lists of tiles
     
 
     def get_val(self, x, y):
         return board[x][y].get_val()
+
+    
+    def change_val(self, x, y, val = 0):
+        """Changes the value stored in board
+        """
+        board[x][y].set_val(val)
 
 
     def new_game(self, difficulty = 0):
--- tile.py
+++ tile.py
@@ -8,6 +8,19 @@ class Tile:
 
     def get_val(self):
         return self.value
+    
+    def set_val(self, val):
+        self.value = val
 
     def get_penciled(self, num):
         return self.penciled[num]
+    
+    def set_penciled(self, num, val = False):
+        """Changes the boolean penciled value of the given number to the boolean val
+        """
+        self.penciled[num] = val
+
+    def switch_penciled(self, num):
+        """Switches the boolean (False->True/True->False) penciled value of the given number for this tile
+        """
+        self.penciled[num] = not self.penciled[num]
